7c69606683fc777cdd8c2ced53a623d7e2e82c81
Ilya Basin <basinilya@gmail.com>
2021-03-04 23:17:02 +0300

    x

--- unix/armbian/heatrobot.sh
+++ unix/armbian/heatrobot.sh
@@ -12,7 +12,7 @@ EMULATE=yes
 
 if [ x"$EMULATE" = x"yes" ]; then
   DELAY_OFF=6
-  DELAY_ON=60
+  DELAY_ON=10
 fi
 
 coproc HUNGP {
@@ -22,9 +22,16 @@ coproc HUNGP {
 STATUS_OK=0
 STATUS_FAIL=1
 
-mysleep() {
+_mysleep() {
   read -t "${1:?}" <&"${HUNGP[0]}" || [ $? = 142 ]
 }
+
+mysleep() {
+  echo "sleeping ${1:?}s"
+  _mysleep "$@"
+}
+
+
 
 set_on() {
   :
@@ -34,15 +41,39 @@ kill_kbd() {
   :
 }
 
-tty_settings=`stty -g`
-trap 'set +e; set_on; kill_kbd 2>/dev/null; stty "$tty_settings"; echo status="on" set, now exiting' EXIT
-stty -icanon -echo
-
-KBD_PID=
+trap 'set +e; set_on; kill_kbd; echo status="on" set, now exiting' EXIT
+function errtrap {     es=$?;     echo "$0: line $1: Command exited with status $es.">&2; }; trap 'errtrap $LINENO' ERR
+set -e
+set -o pipefail
 
 if [ x"$EMULATE" = x"yes" ]; then
-  echo 20 >/dev/shm/heatrobot-temp
 
+  get_temp() {
+    local _temp
+    read _temp </dev/shm/heatrobot-temp
+    temp=$((_temp - 0))
+    if [ $temp = 0 -a x"$_temp" != x"0" ]; then
+      >&2 echo "Invalid integer vailue: $_temp"
+      false
+      exit 1
+    fi
+    #echo "temp=$temp"
+  }
+
+  echo 20000 >/dev/shm/heatrobot-temp
+
+  tty_settings=`stty -g`
+  stty -icanon -echo
+
+  kill_kbd() {
+    {
+      kill $KBD_PID
+      wait $KBD_PID
+    } 2>/dev/null
+    stty "$tty_settings"
+  }
+
+  KBD_PID=
   {
     echo use arrow keys now...
     # 1b5b44 left
@@ -57,10 +88,16 @@ if [ x"$EMULATE" = x"yes" ]; then
           read -r -d "" -n 1 c || break
           case $c in
           $'\x41'|$'\x43') # up or right
-            echo up or right
+            get_temp
+            temp=$((temp + 1000))
+            echo "new temp: $temp"
+            echo $temp >/dev/shm/heatrobot-temp
             ;;
           $'\x42'|$'\x44') # down or left
-            echo down or left
+            get_temp
+            temp=$((temp - 1000))
+            echo "new temp: $temp"
+            echo $temp >/dev/shm/heatrobot-temp
             ;;
           esac
         fi
@@ -68,73 +105,61 @@ if [ x"$EMULATE" = x"yes" ]; then
       # printf '%s' "$c" | xxd
       
     done
-    
-    # xxd -p
   } <&0 &
   KBD_PID=$!
-  kill_kbd() {
-    kill $KBD_PID
-    wait $KBD_PID
-  }
-fi
 
-sleep 10
-
-exit 0
-
-
-function errtrap {     es=$?;     echo "$0: line $1: Command exited with status $es.">&2; }; trap 'errtrap $LINENO' ERR
-set -eE
-set -o pipefail
-
-
-
-if [ x"$EMULATE" = x"yes" ]; then
-
-  get_temp() {
-    local _temp
-    read _temp </dev/shm/heatrobot-temp
-    temp=$((_temp - 0))
-    if [ $temp = 0 && x"$_temp" != x"0" ]; then
-      >&2 echo "Invalid integer vailue: $_temp"
-      false
-      exit 1
-    fi
-    echo "temp=$temp"
-  }
-  
   set_off() {
-    :
+    power_status=$STATUS_FAIL
   }
   
   set_on() {
-    :
+    power_status=$STATUS_OK
   }
   
   is_on() {
-    return $STATUS_OK
+    return $power_status
   }
 
+  set_on
+  
 else
   >&2 echo "Not implemented"
   false
   exit 1
 fi # EMULATE
 
+#exec 0<&-
 
-if is_on; then
-  :
-else
-  :
-fi
-
-get_temp
-
+# set -E # inherit ERR trap
 
 while true; do
-
-
-echo reading from HUNGP
-mysleep 0.2
-
+  if is_on; then
+    mysleep $DELAY_ON
+  else
+    mysleep $DELAY_OFF
+  fi
+  
+  while true; do
+    get_temp
+    
+    if is_on; then
+      if [ $temp -gt $TEMP_MAX ]; then
+        echo "temp=$temp too high, powering off"
+        set_off
+        break
+      else
+        echo "temp=$temp is ok, keep power on"
+      fi
+    else
+      if [ $temp -lt $TEMP_MIN ]; then
+        echo "temp=$temp too low, powering on"
+        set_on
+        break
+      else
+        echo "temp=$temp is ok, keep power off"
+      fi
+    fi
+    
+    _mysleep 2
+  done
 done
