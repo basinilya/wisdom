139ba50a3a24cd34e08b999cce7d9c0450b30494
Ilya Basin <basinilya@gmail.com>
2021-03-04 20:32:33 +0300

    x

--- unix/armbian/heatrobot.sh
+++ unix/armbian/heatrobot.sh
@@ -1,33 +1,74 @@
 #!/bin/bash
 
-TEMP_MIN=15
-TEMP_MAX=24
+TEMP_MIN=15000
+TEMP_MAX=24000
 
 DELAY_OFF=60
 DELAY_ON=600
 
-DELAY_OFF=6
-DELAY_ON=60
+EMULATE=yes
 
-
-function errtrap {     es=$?;     echo "$0: line $1: Command exited with status $es.">&2; }; trap 'errtrap $LINENO' ERR
-set -eE
-set -o pipefail
+if [ x"$EMULATE" = x"yes" ]; then
+  DELAY_OFF=6
+  DELAY_ON=60
+fi
 
 coproc HUNGP {
   read x || true
 }
 
+trap 'set_on; echo status="on" set, now exiting' EXIT
+function errtrap {     es=$?;     echo "$0: line $1: Command exited with status $es.">&2; }; trap 'errtrap $LINENO' ERR
+set -eE
+set -o pipefail
+
 mysleep() {
   read -t "${1:?}" <&"${HUNGP[0]}" || [ $? = 142 ]
 }
 
+STATUS_OK=0
+STATUS_FAIL=1
+
+if [ x"$EMULATE" = x"yes" ]; then
+
+  get_temp() {
+    local _temp
+    read _temp
+    temp=$((_temp - 0))
+    if [ $temp = 0 && x"$_temp" != x"0" ]; then
+      >&2 echo "Invalid integer vailue: $_temp"
+      false
+      exit 1
+    fi
+    echo "temp=$temp"
+  }
+  
+  set_off() {
+    :
+  }
+  
+  set_on() {
+    :
+  }
+  
+  is_on() {
+    return $STATUS_OK
+  }
+
+fi
 
 
+if is_on; then
+  :
+else
+  :
+fi
 
+get_temp
 
 
 while true; do
+
 
 echo reading from HUNGP
 mysleep 0.2
