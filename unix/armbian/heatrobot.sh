f23d1e5ad484358d7950293ac48251ad6c61d1e2
Ilya Basin <basinilya@gmail.com>
2021-03-04 22:45:02 +0300

    x

--- unix/armbian/heatrobot.sh
+++ unix/armbian/heatrobot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+stty sane
+
 TEMP_MIN=15000
 TEMP_MAX=24000
 
@@ -17,23 +19,81 @@ coproc HUNGP {
   read x || true
 }
 
-trap 'set_on; echo status="on" set, now exiting' EXIT
-function errtrap {     es=$?;     echo "$0: line $1: Command exited with status $es.">&2; }; trap 'errtrap $LINENO' ERR
-set -eE
-set -o pipefail
+STATUS_OK=0
+STATUS_FAIL=1
 
 mysleep() {
   read -t "${1:?}" <&"${HUNGP[0]}" || [ $? = 142 ]
 }
 
-STATUS_OK=0
-STATUS_FAIL=1
+set_on() {
+  :
+}
+
+kill_kbd() {
+  :
+}
+
+tty_settings=`stty -g`
+trap 'set +e; set_on; kill_kbd 2>/dev/null; stty "$tty_settings"; echo status="on" set, now exiting' EXIT
+stty -icanon -echo
+
+KBD_PID=
+
+if [ x"$EMULATE" = x"yes" ]; then
+  echo 20 >/dev/shm/heatrobot-temp
+
+  {
+    echo use arrow keys now...
+    # 1b5b44 left
+    # 1b5b43 right
+    # 1b5b41 up
+    # 1b5b42 down
+    
+    while read -r -d "" -n 1 c; do
+      if [ "$c" = $'\x1b' ]; then
+        read -r -d "" -n 1 c || break
+        if [ "$c" = $'[' ]; then
+          read -r -d "" -n 1 c || break
+          case $c in
+          $'\x41'|$'\x43') # up or right
+            echo up or right
+            ;;
+          $'\x42'|$'\x44') # down or left
+            echo down or left
+            ;;
+          esac
+        fi
+      fi
+      # printf '%s' "$c" | xxd
+      
+    done
+    
+    # xxd -p
+  } <&0 &
+  KBD_PID=$!
+  kill_kbd() {
+    kill $KBD_PID
+    wait $KBD_PID
+  }
+fi
+
+sleep 10
+
+exit 0
+
+
+function errtrap {     es=$?;     echo "$0: line $1: Command exited with status $es.">&2; }; trap 'errtrap $LINENO' ERR
+set -eE
+set -o pipefail
+
+
 
 if [ x"$EMULATE" = x"yes" ]; then
 
   get_temp() {
     local _temp
-    read _temp
+    read _temp </dev/shm/heatrobot-temp
     temp=$((_temp - 0))
     if [ $temp = 0 && x"$_temp" != x"0" ]; then
       >&2 echo "Invalid integer vailue: $_temp"
@@ -55,7 +115,11 @@ if [ x"$EMULATE" = x"yes" ]; then
     return $STATUS_OK
   }
 
-fi
+else
+  >&2 echo "Not implemented"
+  false
+  exit 1
+fi # EMULATE
 
 
 if is_on; then
